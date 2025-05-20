82e978f541870a5cdb3ca32b44323249a1188a63
Ava-Rimal <rimalava519@gmail.com>
2025-05-20 13:46:05 +0300

    git commit h5

--- jest-harjoitus1a/t5.productprice.test.js
+++ jest-harjoitus1a/t5.productprice.test.js
@@ -5,15 +5,15 @@ test('ei kanta-asiakas, hinta 40, alv 24%', () => {
 });
 
 test('kanta-asiakas, hinta 100, alv 24%, saa 2.5% alennuksen', () => {
-  expect(productPrice(100, true, 24)).toBe(117.6);
+  expect(productPrice(100, true, 24)).toBe(120.9);  // changed from 117.6 to 120.9
 });
 
 test('kanta-asiakas, hinta 160, alv 14%, saa 5% alennuksen', () => {
-  expect(productPrice(160, true, 14)).toBe(168.96);
+  expect(productPrice(160, true, 14)).toBe(173.28);  // changed from 168.96 to 173.28
 });
 
 test('kanta-asiakas, hinta 300, alv 24%, saa 10% alennuksen', () => {
-  expect(productPrice(300, true, 24)).toBe(259.2);
+  expect(productPrice(300, true, 24)).toBe(334.8);  // changed from 259.2 to 334.8
 });
 
 test('heittää virheen jos syöte on väärä', () => {
